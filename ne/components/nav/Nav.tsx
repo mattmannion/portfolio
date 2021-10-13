06150310cbb9e6e7a8ec1170e3673667025190f3
Matt Mannion <mattpmannion@gmail.com>
2021-10-13 19:22:46 -0400

    added nav routing with accents

--- ne/components/nav/Nav.tsx
+++ ne/components/nav/Nav.tsx
@@ -1,4 +1,21 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+function accent(pathname: string, route: string) {
+  if (pathname === route) return 'nav__accent';
+  else return '';
+}
+
+const routes = {
+  home: '/',
+  projects: '/projects',
+  blog: '/blog',
+  contact: '/contact',
+};
+
 export default function Nav() {
+  const { pathname } = useRouter();
+
   return (
     <>
       <nav className='nav'>
@@ -6,10 +23,18 @@ export default function Nav() {
           <strong>Matt</strong> Mannion
         </div>
         <div className='nav__links'>
-          <div>Home</div>
-          <div>Projects</div>
-          <div>Blog</div>
-          <div>Contact</div>
+          <Link href={routes.home}>
+            <div className={accent(pathname, routes.home)}>Home</div>
+          </Link>
+          <Link href={routes.projects}>
+            <div className={accent(pathname, routes.projects)}>Projects</div>
+          </Link>
+          <Link href={routes.blog}>
+            <div className={accent(pathname, routes.blog)}>Blog</div>
+          </Link>
+          <Link href={routes.contact}>
+            <div className={accent(pathname, routes.contact)}>Contact</div>
+          </Link>
         </div>
         <div className='nav__icons'>
           <div>resume</div>
