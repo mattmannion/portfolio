9ece0c9db308bfbd510b6d709b47415a9d1c6039
Matt Mannion <mattpmannion@gmail.com>
2021-10-20 21:10:07 -0400

    minor changes

--- ne/data/ProjectData.tsx
+++ ne/data/ProjectData.tsx
@@ -16,6 +16,16 @@ export const images = '/images/';
 
 export const ProjectData: ProjectDataIF[] = [
   {
+    slug: 'ea-production-list',
+    img: images + 'ea_pl.png',
+    alt: 'ea production list',
+    title: 'Everything Attachments Production List',
+    desc: `A production list that fetches data from EA's order database and splits each order into individual items before inserting them into a Postgres database for the final list. The final list only contains items made at the EA factory. The list is comprised of 3 parts: the 1rst part shows where the orders are in the manufacturing process, the 2nd part shows items ready to be shipped, and the 3rd and final part shows an archive of all the past shipped orders/items. This project was challenging because Volusion does not allow for Post requests to their database from outside of their native admin panel. To get around this issue, I created a database with Postgres that will store all the relevant orders and update them daily by the overall order number and the item number to ensure the most up-to-date data for the plant to produce their items. This project was connected to EA's database and since I am no longer working there, the majority of the applications update features are no longer functional. This app is a copy of the one used at the plant and is just for show. The user interface is complete with a real-time search for all columns and rows along with pagination, a quick list for items by groups, the ability to add orders by the order number, and more. If this project interests you, please email me for the login information. I will be changing the password and username weekly. One last side note: this project is hosted on Heroku, so it may have a cold start as it wakes.`,
+    github: 'https://github.com/mattmannion/EA-software-suite',
+    host: 'https://ea-software-suite.herokuapp.com/',
+    tech: ['typescript', 'sass', 'postgres', 'expressjs', 'react'],
+  },
+  {
     slug: 'tic-tac-toe',
     img: images + 'ttt.png',
     alt: 'tic-tac-toe snapshot',
